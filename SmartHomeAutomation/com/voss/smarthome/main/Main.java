f127287181a23f3fe28a89a6d81d76f78c7f95a8
Brian <Brian@DESKTOP-8J32Q92.fios-router.home>
2019-10-21 09:24:52 -0600

    Initial Checkin

---
+++ SmartHomeAutomation/com/voss/smarthome/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.voss.smarthome.main;
+
+/**
+ * @author Brian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SmartHomeAutomation/com/voss/smarthome/main/package-info.java
@@ -0,0 +1,1 @@
+package com.voss.smarthome.main;
